21869639a41c1eff7072ac5030971a4cca034543
Sravaniganthala <146628579+Sravaniganthala@users.noreply.github.com>
2024-10-28 08:30:01 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,9 @@
+var a="sravani";
+var age=22;
+var address="vijayawada";
+var pincode=521235;
+console.log(a.length);
+console.log(a);
+console.log(age);
+console.log(address);
+console.log(pincode);
